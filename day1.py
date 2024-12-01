355232a973232cd4e3aabaf49b11c867adff7744
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-01 23:22:01 +0000

    updates

--- day1.py
+++ day1.py
@@ -7,14 +7,22 @@ def parse_input(input):
 
     # return arr
     arr_A = [a.split("   ")[0] for a in arr]
-    arr_b = [a.split("   ")[1] for a in arr]
+    arr_B = [a.split("   ")[1] for a in arr]
 
-    return arr_A, arr_b
+    return arr_A, arr_B
 
 
 def part_one(input):
 
-    pass
+    arr_A, arr_B = input
+
+    arr_A = sorted(arr_A)
+    arr_B = sorted(arr_B)
+    n = len(arr_A)
+
+    diffs = [abs(int(arr_A[i]) - int(arr_B[i])) for i in range(n)]
+
+    return sum(diffs)
 
 
 def part_two(input):
@@ -30,7 +38,7 @@ def main():
 
     sample = []
 
-    # print(part_one(data))
+    print(part_one(data))
     # print(part_two(data))
 
 
