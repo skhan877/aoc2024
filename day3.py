3ae2fa5d7852f1e3594172ba8d14bf64e1518f31
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-06 16:00:24 +0000

    updates

--- day3.py
+++ day3.py
@@ -1,1 +1,35 @@
-"" 
+def parse_input(input): 
+    
+    arr = []
+    with open(input, "r") as f: 
+        for line in f:
+            arr.append(line.replace("\n",""))
+
+    # arr = [row.split(" ") for row in arr] 
+
+    return arr
+
+def part_one(input):
+    pass 
+
+def part_two(input):
+    pass
+
+def main():
+    f = "inputs//day3.txt"
+    data = parse_input(f)
+    # data = data[20:50]
+    # print(data)
+
+    sample = [
+        ]
+
+    # sample = [row.split(" ") for row in sample] 
+    # print(sample)
+
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == "__main__":
+    main()
